d63643d16f92bfd2290f61009f1475a0b00f8dc0
martadelka <oamedvedeva777@gmail.com>
2024-02-10 15:49:28 +0200

    Node.js hw 6

--- controllers/auth.js
+++ controllers/auth.js
@@ -158,7 +158,7 @@ export const updateAvatar = ctrlWrapper(async (req, res) => {
   const resultUpload = path.resolve(avatarsDir, fileName);
 
   const image = await Jimp.read(tempUpload);
-  image.resize(250, 250).write(tempUpload);
+  await image.resize(250, 250).writeAsync(tempUpload);
   await fs.rename(tempUpload, resultUpload);
 
   const avatarURL = path.join("avatars", fileName);
